9add48b1932063657565c60241508948b07aaff2
Vikasrao03 <vikasjadhav88972@gmail.com>
2024-10-08 22:15:49 +0530

    first commit

---
+++ src/StudentForm.java
@@ -0,0 +1,67 @@
+
+public class StudentForm {
+	private String name;
+	private String address;
+	private int phoneNo;
+	private String email;
+	private String dob;
+	private String education;
+	private String gender;
+	private String branch; 
+	private int aadhar;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getAadhar() {
+		return aadhar;
+	}
+	public void setAadhar(int aadhar) {
+		this.aadhar = aadhar;
+	}
+	
+}
---
+++ src/Test.java
@@ -0,0 +1,35 @@
+
+//public class Test {
+//public static void main(String[] args) {
+//		StudentForm stfm1 = new StudentForm();
+//		stfm1.setName("Ajay");
+//		stfm1.setPhoneNo(234);
+//		stfm1.setGender("male");
+//		stfm1.setEmail("ajay@gmail.com");
+//		stfm1.setBranch("CS");
+//		stfm1.setEducation("Btech");
+//		stfm1.setDOB("23/3/2000");
+//		stfm1.setAddress("mumbai");
+//		stfm1.setAadhar(234);
+//		System.out.println(stfm1.getName());
+//		System.out.println(stfm1.getPhoneNo());
+//		System.out.println(stfm1.getGender());
+//		System.out.println(stfm1.getEmail());
+//		System.out.println(stfm1.getEducation());
+//		System.out.println(stfm1.getBranch());
+//		System.out.println(stfm1.getAddress());		
+//		System.out.println(stfm1.getDOB());
+//		System.out.println(stfm1.getAadhar());	
+//		 
+//		StudentForm stfm2 = new StudentForm();
+//		stfm2.setName("anjuu");
+//		stfm2.setPhoneNo(2334);
+//		stfm2.setGender("female");
+//		System.out.println(stfm2.getName());
+//		System.out.println(stfm2.getPhoneNo());
+//		System.out.println(stfm2.getGender());
+//		
+//		
+//	}
+//
+//}
